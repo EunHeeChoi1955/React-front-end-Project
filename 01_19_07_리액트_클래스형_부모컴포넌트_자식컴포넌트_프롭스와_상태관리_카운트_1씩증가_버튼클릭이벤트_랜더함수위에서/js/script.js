0a358a541a87bd6cc7443fee8773091a5165091d
EunHeeChoi1955 <med20001955@naver.com>
2022-01-24 17:03:35 +0900

    220124

---
+++ 01_19_07_리액트_클래스형_부모컴포넌트_자식컴포넌트_프롭스와_상태관리_카운트_1씩증가_버튼클릭이벤트_랜더함수위에서/js/script.js
@@ -0,0 +1,73 @@
+//부모 컴폰너트(Parent Component)
+class MainComponent extends React.Component {
+
+    constructor(props){
+        super(props);
+        this.state = {     //Update 수정 유지 정보관리(상태관리) state
+           cnt: 0   //루트 변경되면 모든 컴포넌트에 전달된다. 즉시 랜더링: 리액티비티
+        }
+    }
+    onClickAddfn = (e) => {
+        e.preventDefault();
+        const {cnt} = this.state;
+        this.setState({cnt: cnt+1});
+    }
+    onClickSubfn = (e) => {
+        e.preventDefault();
+        const {cnt} = this.state;
+        this.setState({cnt: cnt-1});
+    }
+
+    render(){
+
+        const {cnt} = this.state;
+        const {title} = this.props;
+
+        return(
+            <>
+               <section id='section1'>
+                  <div className='container'>
+                    <div className="title" style={{textAlign:'center'}}>
+                        <h1 style={{fontSize:'30px', color:'#c77', textAlign:'center'}}>{title}</h1>
+                        
+                        <button onClick={this.onClickAddfn} style={{cursor:'pointer',fontSize:'20px',backgroundColor:'#222',color:'#fff',padding:'10px 30px'}}>1씩증가</button>&nbsp;
+                        <button onClick={this.onClickSubfn} style={{cursor:'pointer',fontSize:'20px',backgroundColor:'#222',color:'#fff',padding:'10px 30px'}}>1씩감소</button>
+                    </div>  
+                    <div className="content">
+                        <FoodListComponent cnt={cnt}></FoodListComponent>
+                    </div>
+                  </div>
+               </section>
+            </>
+        )
+    }
+}
+
+//자식 컴폰너트(Children Component)
+class FoodListComponent extends React.Component {
+    render(){
+        const {cnt} = this.props;
+        return(
+            <>
+                <ul className='food-list' style={{fontSize:'50px',color:'#000'}}>
+                    {cnt}
+                </ul>  
+            </>
+        )
+    }
+}
+
+// 기본프롭스(defaultProps)
+MainComponent.defaultProps = {
+    title: 'Count Programming',
+    cnt: 0
+}
+
+// 스타일
+
+
+
+ReactDOM.render(
+    <MainComponent>컴포넌트</MainComponent>,
+    document.querySelector('#app')
+);
---
+++ 01_19_09_리액트_클래스형_부모컴포넌트_입력폼데이터_이벤트/js/script.js
@@ -0,0 +1,72 @@
+//부모 컴폰너트(Parent Component)
+class MainComponent extends React.Component {
+
+    constructor(props){
+        super(props);
+        this.state = {     //Update 수정 유지 정보관리(상태관리) state
+           txt: ''   //입력상자 텍스트 할일 내용
+        }
+    }
+
+    onChangefn = (e) => {
+
+       this.setState({txt: e.target.value }) 
+
+    }
+
+
+    render(){
+        const {txt} = this.state;
+        const {title} = this.props;
+
+
+        return(
+            <>
+               <section id='section1'>
+                  <div className='container'>
+                    <div className="title" style={{textAlign:'center'}}>
+                        <h1 style={{fontSize:'30px', color:'#c77', textAlign:'center'}}>{title}</h1>
+                        
+                        <input style={{width:'300px',height:'50px', border:'1px solid #c00'}} value={txt} onChange={this.onChangefn} />
+                        
+                        <button  style={{cursor:'pointer',fontSize:'20px',backgroundColor:'#222',color:'#fff',padding:'10px 30px'}}>1씩증가</button>&nbsp;
+                        <button  style={{cursor:'pointer',fontSize:'20px',backgroundColor:'#222',color:'#fff',padding:'10px 30px'}}>1씩감소</button>
+                    </div>  
+                    <div className="content">
+                        <FoodListComponent txt={txt}></FoodListComponent>
+                    </div>
+                  </div>
+               </section>
+            </>
+        )
+    }
+}
+
+//자식 컴폰너트(Children Component)
+class FoodListComponent extends React.Component {
+    render(){
+        const {txt} = this.props;
+        return(
+            <>
+                <ul className='food-list' style={{fontSize:'50px',color:'#000'}}>
+                    {txt}
+                </ul>  
+            </>
+        )
+    }
+}
+
+// 기본프롭스(defaultProps)
+MainComponent.defaultProps = {
+    title: 'Count Programming',
+    txt: 0
+}
+
+// 스타일
+
+
+
+ReactDOM.render(
+    <MainComponent>컴포넌트</MainComponent>,
+    document.querySelector('#app')
+);
